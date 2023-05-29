6dddb76ee40243ebb97a7151783fd308c03e41c8
aishwary11 <aishwary46@gmail.com>
2023-05-29 19:16:49 +0530

    sorting added

---
+++ recursion/palindrome.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+bool checkPalindrome(string str, int i, int j)
+{
+  if (i > j)
+    return true;
+  if (str[i] != str[j])
+  {
+    return false;
+  }
+  else
+  {
+    return checkPalindrome(str, i + 1, j - 1);
+  }
+}
+
+int main()
+{
+  string str = "abbac";
+  cout << endl;
+  bool isPalindrome = checkPalindrome(str, 0, str.length() - 1);
+  if (isPalindrome)
+  {
+    cout << "It is Palindrome" << endl;
+  }
+  else
+  {
+    cout << "Not a Palindrome" << endl;
+  }
+  return 0;
+}
